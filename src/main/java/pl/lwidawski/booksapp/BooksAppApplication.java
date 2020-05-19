85ed5ada2180a4ca129a33759e116103a2c869c1
LWidawski <3acc2297@gmail.com>
2020-05-19 19:20:15 +0200

    init

---
+++ src/main/java/pl/lwidawski/booksapp/BooksAppApplication.java
@@ -0,0 +1,13 @@
+package pl.lwidawski.booksapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooksAppApplication.class, args);
+    }
+
+}
