3613c08459b328686d8242c834355d9f9b8933c8
LWidawski <3acc2297@gmail.com>
2020-05-19 19:33:52 +0200

    book pojo

---
+++ src/main/java/pl/lwidawski/booksapp/Book.java
@@ -0,0 +1,34 @@
+package pl.lwidawski.booksapp;
+
+import java.time.LocalDate;
+
+public class Book {
+
+    private Long id;
+    private String title;
+    private LocalDate publicationYear;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public LocalDate getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(LocalDate publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+}
